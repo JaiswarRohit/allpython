8cdd6f2f470f7f3334d4dbdd5d5ca90f41cbff1c
Rohit Jaiswar <rjrohit1306@gmail.com>
2024-02-22 03:24:16 +0530

    add

---
+++ OOPs/testclass.py
@@ -0,0 +1,10 @@
+class test:
+    def rohit(self):
+        print(f'rohit testing the oops')
+
+    def nitin(self):
+        print (f'nitin Explain the oops')
+
+oops = test()
+oops.rohit()
+oops.nitin()
