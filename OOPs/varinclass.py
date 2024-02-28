2c976d26aa8fa826c09ca755830c90ba36a3d696
Rohit Jaiswar <rjrohit1306@gmail.com>
2024-02-29 04:05:06 +0530

    variable type

---
+++ OOPs/varinclass.py
@@ -0,0 +1,34 @@
+# Instance Variables
+"""
+class Student:
+    def __init__(self, name, id):
+        self.name=name
+        self.id=id
+
+s1 = Student('rohit', 1)
+s2 = Student('suman', 2)
+print("Student1 info: ")
+print("Name: ", s1.name)
+print("id: ", s1.id)
+print("Student2 info: ")
+print("Name: ", s2.name)
+print("id: ", s2.id)
+
+
+# Instance Variable in a Constructor
+
+class Employee:
+    def __init__(self):
+        self.eno = 1
+        self.ename = "Rohit"
+
+e = Employee()
+print("Employee number: ", e.eno)
+print("Employee Name: ", e.ename)
+print(e.__dict__)
+
+"""
+
+#Declaring Instance variable using instance methods
+
+
